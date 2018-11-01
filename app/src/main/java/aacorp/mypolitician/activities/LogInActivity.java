40727dc03a371d0f7b2a2d6ae9526211e29a7683
Alex Smythe <alex.krogh.smythe@post.au.dk>
2018-11-01 16:21:00 +0100

    login done

---
+++ app/src/main/java/aacorp/mypolitician/activities/LogInActivity.java
@@ -0,0 +1,101 @@
+package aacorp.mypolitician.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+import aacorp.mypolitician.R;
+
+public class LogInActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    TextView t2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        if(user==null){
+            setContentView(R.layout.activity_log_in);
+            FacebookSdk.sdkInitialize(getApplicationContext());
+            loginButton = (LoginButton) findViewById(R.id.login_button);
+            callbackManager = CallbackManager.Factory.create();
+            loginButton.setReadPermissions(Arrays.asList("email"));
+            t2 = (TextView) findViewById(R.id.textView2);
+        }
+
+    }
+
+    public void buttonClickLoginFB(View v){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        handleFacebookToken(loginResult.getAccessToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getApplicationContext(), "Cancelled by User", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(getApplicationContext(),"Error error pants on ferror", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser myUserObj = mAuth.getCurrentUser();
+                    updateUI(myUserObj);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Could not register to firebase skrt", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void updateUI(FirebaseUser myUserObj) {
+        t2.setText(myUserObj.getEmail());
+    }
+
+}
--- app/src/main/java/aacorp/mypolitician/activities/Match.java
+++ app/src/main/java/aacorp/mypolitician/activities/Match.java
@@ -1,7 +1,7 @@
 package aacorp.mypolitician.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import aacorp.mypolitician.R;
 
@@ -11,5 +11,7 @@ public class Match extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+
     }
 }
