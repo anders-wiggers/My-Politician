ed81fecdb2b1d0dc50d3e0b0f91b43ba0302c1dd
Anders Wiggers <anders.bille.wiggers@post.au.dk>
2018-11-06 15:22:52 +0100

    added button main to activitys and matches list

--- app/src/main/java/aacorp/mypolitician/patterns/Database.java
+++ app/src/main/java/aacorp/mypolitician/patterns/Database.java
@@ -1,13 +1,19 @@
 package aacorp.mypolitician.patterns;
 
-class Database {
+import com.google.firebase.firestore.FirebaseFirestore;
 
+class Database {
     private static final Database instance = new Database();
+    private FirebaseFirestore db;
 
     private Database(){}
 
     public static Database getInstance(){
         return instance;
+    }
+
+    public FirebaseFirestore getDatabase(){
+        return db = FirebaseFirestore.getInstance();
     }
 
 
